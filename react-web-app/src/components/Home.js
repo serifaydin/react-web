2a4a76e97d4d2fdf767736962b794bb768b2b5eb
Serif Aydin <serifaydin99@gmail.com>
2021-10-25 17:18:02 +0300

    dd
    ddd

---
+++ react-web-app/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home =()=>{
+    return(
+        <div className="container">
+            <h4 className="center">Anasayfa</h4>
+            <p>Anasayfa yazıları</p>
+        </div>
+    )
+}
+
+export default Home;
