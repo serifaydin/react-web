ee30fe469f72a3a5c01f1ad4645c5b05264bf02b
Serif Aydin <serifaydin99@gmail.com>
2021-10-25 23:40:40 +0300

    route full
    route full

--- react-web-app/src/App.js
+++ react-web-app/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Navbar from './components/Navbar.js'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Post from './components/Post'
 
-class App extends React.Component{
-  render(){
-    return(
-      <div className="App">
-
-      </div>
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/about" component={About} />
+            <Route path="/:postId" component={Post} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     )
   }
 }
---
+++ react-web-app/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact =()=>{
+    return(
+        <div className="container">
+            <h4 className="center">İletişim</h4>
+            <p>İletişim yazıları</p>
+        </div>
+    )
+}
+
+export default Contact;
