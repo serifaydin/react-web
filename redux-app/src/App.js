7d2a0b9b02b268f85aae3db086cbebb363b3aa5c
Serif Aydin <serifaydin99@gmail.com>
2021-10-26 21:55:36 +0300

    redux
    redux

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    this.handleIsimUpdate = this.handleIsimUpdate.bind(this);
+  }
+
+  handleIsimUpdate() {
+    const updateIsimVerileri = (item)=>{
+      return{
+        type:"ISIM_GUNCELLE",
+        payload:item
+      }
+    }
+
+    this.props.dispatch(updateIsimVerileri('Deneme App'));
+  }
+
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleIsimUpdate}>Güncelle</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  console.log(state);
+  return state
+}
+
+export default connect(mapStateToProps)(App);
--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { combineReducers, createStore } from 'redux'
+import {Provider} from 'react-redux'
+
+function isimReducer(state = 'ali', action) {
+
+  switch (action.type) {
+    case 'ISIM_GUNCELLE':
+      return action.payload;
+    default:
+
+      return state
+  }
+}
+
+function soyisimReducer(state = 'hazır', action) {
+  switch (action.type) {
+    case 'SOYISIM_GUNCELLE':
+      return action.payload;
+    default:
+
+      return state
+  }
+}
+
+const updateIsimVerileri = {
+  type: "ISIM_GUNCELLE",
+  payload: "ali osman"
+}
+
+const updateSoyisimVerileri = {
+  type: "SOYISIM_GUNCELLE",
+  payload: "hazir2"
+}
+
+const reducers = combineReducers({
+  isim: isimReducer,
+  soyisim: soyisimReducer
+})
+
+const store = createStore(reducers);
+store.dispatch(updateIsimVerileri);
+store.dispatch(updateSoyisimVerileri);
+
+console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}><App /></Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
